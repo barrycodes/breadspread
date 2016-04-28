295ceec2aa8584247f3216d0378e97fde68382ab
Barry Smith <bsmith.has.email@gmail.com>
2016-04-28 14:45:03 -0500

    changed data type of Email field in viewmodel

--- BreadSpread/BreadSpread.Web/Models/AccountViewModels.cs
+++ BreadSpread/BreadSpread.Web/Models/AccountViewModels.cs
@@ -108,7 +108,6 @@ namespace BreadSpread.Web.Models
     public class ForgotPasswordViewModel
     {
         [Required]
-        [EmailAddress]
         [Display(Name = "Username or Email")]
         public string Email { get; set; }
     }
