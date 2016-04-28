15bab594dc01f019243d2d47c7d06af70776dff2
Barry Smith <bsmith.has.email@gmail.com>
2016-04-28 12:39:34 -0500

    changed password requirements

--- BreadSpread/BreadSpread.Web/App_Start/IdentityConfig.cs
+++ BreadSpread/BreadSpread.Web/App_Start/IdentityConfig.cs
@@ -53,7 +53,7 @@ namespace BreadSpread.Web
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
+                RequiredLength = 8,
                 RequireNonLetterOrDigit = true,
                 RequireDigit = true,
 				//RequireLowercase = true,
