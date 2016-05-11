2739545ecaed02c4b84025b50c60b45442549453
Barry Smith <bsmith.has.email@gmail.com>
2016-05-11 14:01:15 -0500

    moved some code to new Invitation controller

--- BreadSpread/BreadSpread.Web/Controllers/GroupController.cs
+++ BreadSpread/BreadSpread.Web/Controllers/GroupController.cs
@@ -16,7 +16,7 @@ namespace BreadSpread.Web.Controllers
 	[Authorize]
     public class GroupController : Controller
     {
-        private ApplicationIdentityDbContext db = new ApplicationIdentityDbContext();
+        private ApplicationIdentityDbContext db;
 
 		/// <summary>
 		/// User manager - attached to application DB context
@@ -25,6 +25,7 @@ namespace BreadSpread.Web.Controllers
 		
 		public GroupController()
 		{
+			db = new ApplicationIdentityDbContext();
 			UserManager = new UserManager<User>(new UserStore<User>(db));
 		}
 
@@ -273,81 +274,6 @@ namespace BreadSpread.Web.Controllers
 			await db.SaveChangesAsync();
 
 			return new HttpStatusCodeResult(HttpStatusCode.OK);
-		}
-
-		public ActionResult Invite(string groupId, string groupName)
-		{
-			GroupInviteViewModel model = new GroupInviteViewModel();
-
-			model.GroupId = groupId;
-			model.GroupName = groupName;
-			model.FromUsername = User.Identity.GetUserName();
-
-			return View(model);
-		}
-
-		private async Task SendEmailInvite(string emailAddress, Invitation invitation, string groupName, string fromUsername)
-		{
-			// For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-			// Send an email with this link
-
-			var callbackUrl =
-				Url.Action(
-					"AnswerInvite",
-					"Group",
-					new { invitationId = invitation.Id },
-					protocol: Request.Url.Scheme);
-
-			string message =
-				"BreadSpread Group Invitation<br/><br/>"
-				+ "You've been invited to join a group on BreadSpread by user "
-				+ fromUsername
-				+ ".<br/><br/>"
-				+ "Group name:"
-				+ groupName
-				+ "<br/><br/>"
-				+ "Click <a href=\""
-				+ callbackUrl
-				+ "\">here</a> to join the group.<br/><br/>"
-				+ "If the above link doesn't work, please copy and paste "
-				+ "the following into your address bar and press Enter:<br/><br/>"
-				+ callbackUrl;
-
-			await EmailService.SendEmailAsync(emailAddress, "deep.cosmic.mysteries@gmail.com", "BreadSpread Group Invitation", message);
-		}
-
-		[HttpPost]
-		public async Task<ActionResult> Invite(GroupInviteViewModel model)
-		{
-			Invitation invitation = new Invitation();
-			Group group = await db.Groups.FirstOrDefaultAsync(g => g.Id == model.GroupId);
-			if (group == null)
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-			invitation.Group = group;
-			invitation.Id = Guid.NewGuid().ToString();
-			invitation.CreatedTime = DateTime.Now;
-			db.Invitations.Add(invitation);
-			await db.SaveChangesAsync();
-
-			string destEmail = model.Username;
-			var destUser = await UserManager.FindByNameAsync(model.Username);
-			if (destUser != null)
-				destEmail = destUser.Email;
-
-			await SendEmailInvite(destEmail, invitation, group.Name, model.FromUsername);
-
-			return RedirectToAction("InviteSent");
-		}
-
-		public ActionResult InviteSent()
-		{
-			return View();
-		}
-
-		public ActionResult AnswerInvite()
-		{
-			return View();
 		}
 
 		protected override void Dispose(bool disposing)
---
+++ BreadSpread/BreadSpread.Web/Controllers/InvitationController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using BreadSpread.Web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BreadSpread.Web.Controllers
+{
+	[Authorize]
+    public class InvitationController : Controller
+    {
+		private ApplicationIdentityDbContext db;
+
+		/// <summary>
+		/// User manager - attached to application DB context
+		/// </summary>
+		protected UserManager<User> UserManager { get; set; }
+
+		public InvitationController()
+		{
+			db = new ApplicationIdentityDbContext();
+			UserManager = new UserManager<User>(new UserStore<User>(db));
+		}
+
+		//// GET: Invitation
+		//public ActionResult Index()
+		//{
+		//    return View();
+		//}
+
+		private async Task SendEmailInvite(string emailAddress, Invitation invitation, string groupName, string fromUsername)
+		{
+			// For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+			// Send an email with this link
+
+			var callbackUrl =
+				Url.Action(
+					"AnswerInvite",
+					"Invitation",
+					new { invitationId = invitation.Id },
+					protocol: Request.Url.Scheme);
+
+			string message =
+				"BreadSpread Group Invitation<br/><br/>"
+				+ "You've been invited to join a group on BreadSpread by user "
+				+ fromUsername
+				+ ".<br/><br/>"
+				+ "Group name:"
+				+ groupName
+				+ "<br/><br/>"
+				+ "Click <a href=\""
+				+ callbackUrl
+				+ "\">here</a> to join the group.<br/><br/>"
+				+ "If the above link doesn't work, please copy and paste "
+				+ "the following into your address bar and press Enter:<br/><br/>"
+				+ callbackUrl;
+
+			await EmailService.SendEmailAsync(emailAddress, "deep.cosmic.mysteries@gmail.com", "BreadSpread Group Invitation", message);
+		}
+
+		public ActionResult Invite(string groupId, string groupName)
+		{
+			GroupInviteViewModel model = new GroupInviteViewModel();
+
+			model.GroupId = groupId;
+			model.GroupName = groupName;
+			model.FromUsername = User.Identity.GetUserName();
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Invite(GroupInviteViewModel model)
+		{
+			Invitation invitation = new Invitation();
+			Group group = await db.Groups.FirstOrDefaultAsync(g => g.Id == model.GroupId);
+			if (group == null)
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			invitation.Group = group;
+			invitation.Id = Guid.NewGuid().ToString();
+			invitation.CreatedTime = DateTime.Now;
+			db.Invitations.Add(invitation);
+			await db.SaveChangesAsync();
+
+			string destEmail = model.Username;
+			var destUser = await UserManager.FindByNameAsync(model.Username);
+			if (destUser != null)
+				destEmail = destUser.Email;
+
+			await SendEmailInvite(destEmail, invitation, group.Name, model.FromUsername);
+
+			return RedirectToAction("InviteSent");
+		}
+
+		public ActionResult InviteSent()
+		{
+			return View();
+		}
+
+		public ActionResult AnswerInvite()
+		{
+			return View();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
